afcf153ab6a1c661966201c8597dd8a3f3099dfb
prathamc9221 <96642687+prathamc9221@users.noreply.github.com>
2023-12-09 09:56:37 -0500

    Update detect.py

--- Object-Detection/detect.py
+++ Object-Detection/detect.py
@@ -7,6 +7,7 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
+
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
